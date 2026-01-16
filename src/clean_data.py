3a06564e278543827fbd21cee3f12d99e5bd88ec
Ali Zaidi <syedalzd772@gmail.com>
2026-01-15 23:49:03 -0700

    Added dataset to workspace

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,2 +1,15 @@
 
+def convert_col_types(df):
+
+    colNames = {
+
+        "transaction_date": "datetime",
+        "amount_spent": "float",
+        "quantity": "int",
+        "product_category": "category"
+
+    }
+
+
+                  
 
